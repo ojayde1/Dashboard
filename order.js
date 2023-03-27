545e2d9d6704249055822fe29bd548cdabecdd4f
Olajide <ojayde35@gmail.com>
2023-03-27 16:09:50 +0100

    first commit

---
+++ order.js
@@ -0,0 +1,33 @@
+const Orders = [
+     {
+       productName:"Apple Macbook pro",
+       productNumber:"56743",
+       paymentStatus:"Due",
+       shipping:"Pending"
+     },
+     {
+          productName:"Iphone Xr",
+          productNumber:"46290",
+          paymentStatus:"Paid",
+          shipping:"Declined"
+     },
+     {
+          productName:"Airpods pro 2",
+          productNumber:"35356",
+          paymentStatus:"Due",
+          shipping:"Delivered"
+     },
+     {
+          productName:"Brown ZTE",
+          productNumber:"76643",
+          paymentStatus:"Paid",
+          shipping:"Pending"
+        },
+        {
+          productName:"Huawei Charger",
+          productNumber:"21448",
+          paymentStatus:"Due",
+          shipping:"Cancelled"
+        }  
+
+]
